321ec23feb39cba507a3925e4ac12e1be159136d
TB-ME <thomascbentham@gmail.com>
2024-08-08 15:07:59 +1000

    use rustc_version to determine if rustc channel.

--- build.rs
+++ build.rs
@@ -6,23 +6,12 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-// It would be nice to use the rustversion crate here instead,
-// but that doesn't work with inner attributes.
 fn main() {
-    println!("cargo::rustc-check-cfg=cfg(nightly)");
-    if let Some(ver) = rustc_version() {
-        if ver.contains("nightly") {
-            println!("cargo:rustc-cfg=nightly")
-        }
+    if Some(rustc_version::Channel::Nightly) == version_meta() {
+        println!("cargo:rustc-cfg=nightly");
     }
 }
 
-fn rustc_version() -> Option<String> {
-    let rustc = std::env::var_os("RUSTC")?;
-    let output = std::process::Command::new(rustc)
-        .arg("--version")
-        .output()
-        .ok()?;
-    let version = String::from_utf8(output.stdout).ok()?;
-    Some(version)
+fn version_meta() -> Option<rustc_version::Channel> {
+    Some(rustc_version::version_meta().ok()?.channel)
 }
